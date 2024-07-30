1e409cd19e3faa29ec799e6102d695d1ffcb0c02
redCodev <redcodev@icloud.com>
2024-07-30 13:22:57 -0300

    refactoring: DRY method && best practices

--- src/script.js
+++ src/script.js
@@ -1,131 +1,130 @@
 $(document).ready(function () {
-	var $pix = $('#pix')
-	var $card = $('#card')
-	var $cardKey = $('#cardKey')
-	var $totalPix = $('#totalPix')
-	var $totalCard = $('#totalCard')
-	var $cardOwner = $('#cardOwner')
-	var $cardNumber = $('#cardNumber')
+	const $pix = $('#pix')
+	const $card = $('#card')
+	const $cardKey = $('#cardKey')
+	const $totalPix = $('#totalPix')
+	const $totalCard = $('#totalCard')
+	const $cardOwner = $('#cardOwner')
+	const $cardNumber = $('#cardNumber')
+	const $errorCardNumber = $('#errorCardNumber')
+	const $usdCircle = $('#usdCircle')
+	const $dollar = $('#dollar')
+	const $selectParcels = $('#selectParcels')
+	const $cpf = $('#cpf')
+
+	function toggleDisplay($element, display) {
+		$element.css('display', display)
+	}
+
+	function updateCardDisplay(valor) {
+		$totalCard.html(valor.toFixed(2))
+		$('#1x').html('1x R$' + valor.toFixed(2))
+		$('#2x').html('2x R$' + (valor / 2).toFixed(2))
+		$('#3x').html('3x R$' + (valor / 3).toFixed(2))
+		$('#4x').html('4x R$' + ((valor + valor * 0.05) / 4).toFixed(2))
+		$('#5x').html('5x R$' + ((valor + valor * 0.1) / 5).toFixed(2))
+	}
+
+	function validateInput(inputValue, pattern) {
+		return pattern.test(inputValue)
+	}
+
+	function validateNumberInput(event) {
+		const controlKeys = [8, 9, 37, 39, 46]
+		const isControlKey = controlKeys.includes(event.which)
+		if ((event.which < 48 || event.which > 57) && !isControlKey) {
+			event.preventDefault()
+		}
+	}
+
+	function validateLetterInput(event) {
+		const controlKeys = [8, 9, 32, 37, 39, 46]
+		const isControlKey = controlKeys.includes(event.which)
+		if ((event.which < 65 || event.which > 90) && !isControlKey) {
+			event.preventDefault()
+		}
+	}
+
+	function handlePaymentMethod(valor) {
+		if ($('#radioPix').is(':checked')) {
+			toggleDisplay($pix, 'block')
+			toggleDisplay($card, 'none')
+			$totalPix.html((valor - valor * 0.1).toFixed(2))
+		} else if ($('#radioCard').is(':checked')) {
+			toggleDisplay($card, 'block')
+			toggleDisplay($pix, 'none')
+			updateCardDisplay(valor)
+
+			$selectParcels.on('change', function () {
+				const selectedOpt = $(this).val()
+				if (selectedOpt === '4x') {
+					$totalCard.html((valor + valor * 0.05).toFixed(2))
+				} else if (selectedOpt === '5x') {
+					$totalCard.html((valor + valor * 0.1).toFixed(2))
+				} else {
+					$totalCard.html(valor.toFixed(2))
+				}
+			})
+		} else {
+			alert('Selecione um método de pagamento.')
+		}
+	}
 
 	$('#btnDetails').click(function () {
-		// Check if the shopping value is provided
-		var userInput = $('#textField').val()
+		const userInput = $('#textField').val()
 		if (userInput === '') {
 			alert("Por favor, preencha o campo 'Valor'.")
 			return
-		} else {
-			if (!/^\d+(\.\d+)?$/.test(userInput)) {
-				alert('Por favor, insira um número válido.')
-				return
-			} else {
-				var valor = parseFloat(userInput)
-				// Check the payment method
-				if ($('#radioPix').is(':checked')) {
-					$pix.css('display', 'block')
-					$card.css('display', 'none')
-					// Set the informed value to the total with a 10% discount
-					$totalPix.html((valor - valor * 0.1).toFixed(2))
-				} else if ($('#radioCard').is(':checked')) {
-					$card.css('display', 'block')
-					$pix.css('display', 'none')
-					// Set the informed value to the total
-					$totalCard.html(valor.toFixed(2))
-					// Display the amount of each parcel according to the total
-					$('#1x').html('1x R$' + valor.toFixed(2))
-					$('#2x').html('2x R$' + (valor / 2).toFixed(2))
-					$('#3x').html('3x R$' + (valor / 3).toFixed(2))
-					// Add interest to the parcels
-					$('#4x').html('4x R$' + ((valor + valor * 0.05) / 4).toFixed(2))
-					$('#5x').html('5x R$' + ((valor + valor * 0.1) / 5).toFixed(2))
-					// Change the total price display in case of interest on the parcels
-					$('#selectParcels').on('change', function () {
-						var selectedOpt = $(this).val()
-						if (
-							selectedOpt == '1x' ||
-							selectedOpt == '2x' ||
-							selectedOpt == '3x'
-						) {
-							$totalCard.html(valor.toFixed(2))
-						}
-						if (selectedOpt == '4x') {
-							$totalCard.html((valor + valor * 0.05).toFixed(2))
-						}
-						if (selectedOpt == '5x') {
-							$totalCard.html((valor + valor * 0.1).toFixed(2))
-						}
-					})
-				} else {
-					alert('Selecione um método de pagamento')
-				}
-			}
 		}
-	})
-	// Only allow numeric and control keys
-	$cardNumber.keydown(function (event) {
-		var controlKeys = [8, 9, 37, 39, 46] // backspace, tab, left arrow, right arrow, delete
-		var isControlKey = controlKeys.indexOf(event.which) !== -1
-		// Check if it's a number or a control key
-		if ((event.which < 48 || event.which > 57) && !isControlKey) {
-			event.preventDefault() // Prevent the default action (typing)
+		if (!/^\d+(\.\d+)?$/.test(userInput)) {
+			alert('Por favor, insira um número válido.')
+			return
 		}
+		const valor = parseFloat(userInput)
+		handlePaymentMethod(valor)
 	})
 
-	// Verify the card's flag
+	$cardNumber.on('keydown', validateNumberInput)
+
 	$cardNumber.on('input', function () {
-		var inputValue = $(this).val()
-		var pattern1 = /^1234/
-		var pattern2 = /^4321/
-		if (pattern1.test(inputValue)) {
-			$('#usdCircle').css('display', 'contents')
-			$('#dollar').css('display', 'none')
-		} else if (pattern2.test(inputValue)) {
-			$('#dollar').css('display', 'contents')
-			$('#usdCircle').css('display', 'none')
+		const inputValue = $(this).val()
+		if (validateInput(inputValue, /^1234/)) {
+			toggleDisplay($usdCircle, 'contents')
+			toggleDisplay($dollar, 'none')
+		} else if (validateInput(inputValue, /^4321/)) {
+			toggleDisplay($dollar, 'contents')
+			toggleDisplay($usdCircle, 'none')
 		} else {
-			$('#usdCircle').css('display', 'none')
-			$('#dollar').css('display', 'none')
+			toggleDisplay($dollar, 'none')
+			toggleDisplay($usdCircle, 'none')
 		}
 	})
-	// Check for invalid card number
+
 	$cardNumber.on('focusout', function () {
-		var inputValue = $(this).val()
-		var pattern1 = /^1234/
-		var pattern2 = /^4321/
-		if (!pattern1.test(inputValue) && !pattern2.test(inputValue)) {
-			$('#errorCardNumber').css('display', 'contents')
-			$('#errorCardNumber').css('color', 'red')
-		} else {
-			$('#errorCardNumber').css('display', 'none')
-		}
-	})
-	// Only allow (upper case) letters
-	$cardOwner.keydown(function (event) {
-		var controlKeys = [8, 9, 32, 37, 39, 46]
-		var isControlKey = controlKeys.indexOf(event.which) !== -1
-		if ((event.which < 65 || event.which > 90) && !isControlKey) {
-			event.preventDefault()
-		} else {
-			$(this).on('input', function () {
-				var valUpper = $(this).val().toUpperCase()
-				$(this).val(valUpper)
-			})
-		}
-	})
-
-	$cardKey.keydown(function (event) {
-		var controlKeys = [8, 9, 37, 39, 46]
-		var isControlKey = controlKeys.indexOf(event.which) !== -1
-		if ((event.which < 48 || event.which > 57) && !isControlKey) {
-			event.preventDefault()
-		}
-	})
-
-	$('#submitCard').on('click', function () {
-		let card = $cardNumber.val()
-		let owner = $cardOwner.val()
-		let key = $cardKey.val()
+		const inputValue = $(this).val()
 		if (
-			$('#errorCardNumber').css('display') !== 'none' ||
+			!validateInput(inputValue, /^1234/) &&
+			!validateInput(inputValue, /^4321/)
+		) {
+			$errorCardNumber.css({ display: 'contents', color: 'red' })
+		} else {
+			$errorCardNumber.css('display', 'none')
+		}
+	})
+
+	$cardOwner.on('keydown', validateLetterInput).on('input', function () {
+		const valUpper = $(this).val().toUpperCase()
+		$(this).val(valUpper)
+	})
+
+	$cardKey.on('keydown', validateNumberInput)
+
+	$('#submitCard').click(function () {
+		const card = $cardNumber.val()
+		const owner = $cardOwner.val()
+		const key = $cardKey.val()
+		if (
+			$errorCardNumber.css('display') !== 'none' ||
 			owner == '' ||
 			key == ''
 		) {
@@ -135,17 +134,11 @@ $(document).ready(function () {
 		}
 	})
 
-	$('#cpf').keydown(function (event) {
-		var controlKeys = [8, 9, 37, 39, 46]
-		var isControlKey = controlKeys.indexOf(event.which) !== -1
-		if ((event.which < 48 || event.which > 57) && !isControlKey) {
-			event.preventDefault()
-		}
-	})
+	$cpf.on('keydown', validateNumberInput)
 
-	$('#submitPix').on('click', function () {
-		let cpf = $('#cpf').val()
-		if (cpf == '') {
+	$('#submitPix').click(function () {
+		const cpf = $cpf.val()
+		if (cpf === '') {
 			alert('Por favor, informe o CPF.')
 		} else {
 			alert('Pagamento efetuado com sucesso!')
